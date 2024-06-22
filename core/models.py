e0965c8e2b7c3c7674b5f36004ceef7327cf3c48
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-06-23 02:18:50 +0300

    Накатил первую миграцию на БД. Создал в БД таблицу Actions. Установил связь с Users

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 import datetime
+from typing import List
+
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.types import BigInteger, String, DateTime
-from sqlalchemy import func
+from sqlalchemy import func, ForeignKey
 
 
 class BaseModel(AsyncAttrs, DeclarativeBase):
@@ -23,5 +25,32 @@ class User(BaseModel):
     fullname: Mapped[str | None] = mapped_column(String(200), nullable=True)
     nickname: Mapped[str | None] = mapped_column(String(200), nullable=True)
 
+    actions: Mapped[List["Action"]] = relationship(back_populates="Users", cascade="all, delete")
+
     def __repr__(self):
         return f"{self.__class__.__name__} ({self.telegram_id=}; {self.fullname=}; {self.nickname=})"
+
+
+class Action(BaseModel):
+
+    __tablename__ = "Actions"
+
+    id: Mapped[int] = mapped_column(BigInteger,
+                                    primary_key=True,
+                                    autoincrement=True,
+                                    index=True,
+                                    unique=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.datetime.now(),
+        server_default=func.now()
+    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.telegram_id",
+                                                    ondelete="CASCADE"))
+    message: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    details: Mapped[str | None] = mapped_column(String, nullable=True)
+    user: Mapped["User"] = relationship(back_populates="Actions")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} ({self.created_at=} ; {self.user_id=} ; {self.message=} ; {self.details=})"
