e0a1e6065f34ebb7c93abb5d88a48690a29fd637
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-06-23 01:29:54 +0300

    Создал исполняемый модуль, содержащий в себе точку входа в приложение

---
+++ bot.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.methods import DeleteWebhook
+from aiogram.client.default import DefaultBotProperties
+
+from config import Config
+
+
+dp = Dispatcher()
+properties = DefaultBotProperties(parse_mode=ParseMode.HTML)
+
+
+async def main() -> None:
+    bot = Bot(
+        token=Config.Bot.TOKEN,
+        default=properties
+    )
+    await bot(DeleteWebhook(drop_pending_updates=True))
+    await dp.start_polling(
+        bot,
+        allowed_updates=dp.resolve_used_update_types()
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=Config.Logging.LEVEL,
+        stream=sys.stdout,
+        format=Config.Logging.FORMAT
+    )
+    asyncio.run(main())
